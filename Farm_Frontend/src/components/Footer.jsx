758a1bfde94e17c3263c69ab55dea478e543f7c9
Molinda Mihiranga <it22073396@my.sliit.lk>
2025-05-02 20:18:28 +0530

    footer changes

--- Farm_Frontend/src/components/Footer.jsx
+++ Farm_Frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 // Footer.js
 import React from 'react';
-import mandriLifeLogo from '../assets/mandri-logo_white.png'; // Import your Mandri Life logo
+import Logo from '../assets/logo.jpg'; // Import your Mandri Life logo
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
@@ -9,11 +9,11 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="col-md-3 text-left">
-            <img src={mandriLifeLogo} alt="Mandri Life Logo" style={{ maxWidth: '100px' }} />
-            <p>At Mandri Lanka (Private) Ltd we love to hear your views about our Mandri products. We wish to know how you feel about our products, your suggestion and complain.</p>
+            <img src={Logo} alt="Logo" style={{ maxWidth: '100px' }} />
+            <p>At Farm Management(Private) Ltd we love to hear your views about our products. We wish to know how you feel about our products, your suggestion and complain.</p>
           </div>
           <div className="col-md-3">
-            <p>Mandri Life</p>
+            <p>Farm Management</p>
             <ul className="list-unstyled">
               <li>Home</li>
               <li>Vision & Mission</li>
@@ -26,20 +26,20 @@ const Footer = () => {
           <div className="col-md-3">
             <p>Products</p>
             <ul className="list-unstyled">
-              <li>Spices</li>
-              <li>Essential Oil</li>
-              <li>Herbal Products</li>
+              <li>Milk</li>
+              <li>Cheese</li>
+              <li>waste soil</li>
              <Link to="/createfeedbacks"><p>Feedback</p></Link> 
             </ul>
           </div>
           <div className="col-md-3">
             <p>Contact us</p>
             <p>+94 77 171 3375</p>
-            <p>info@mandrilife.com</p>
-            <p>Mandri Lanka Private Limited, No 227/13 A, Nirmana Mw, Nawala Road, Nugegoda, Sri Lanka</p>
+            <p>info@farmmanagement.com</p>
+            <p>Farm Management Private Limited, No 227/13 A, Nirmana Mw, Nawala Road, Nugegoda, Sri Lanka</p>
           </div>
         </div>
-        <p>&copy; {new Date().getFullYear()} Mandri Life. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Farm Management. All rights reserved.</p>
       </div>
     </footer>
   );
